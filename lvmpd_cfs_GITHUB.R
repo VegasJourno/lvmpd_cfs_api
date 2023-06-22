db741ea26178ca8b3cd8ac57a38b9d24d02afc7b
Michael Scott Davidson <lvrjautodata@gmail.com>
2023-06-22 11:26:21 -0700

    Update lvmpd_cfs_GITHUB.R

--- lvmpd_cfs_GITHUB.R
+++ lvmpd_cfs_GITHUB.R
@@ -158,7 +158,7 @@ knitr::knit2html("lvmpd_cfs_markdown.Rmd",options="")
 
 #Email notification of success
 send.mail(from = GMAIL_SENDER,
-          to = GMAIL_SENDER,
+          to = c("crimeteam@reviewjournal.com"),
           subject = paste0("LVMPD CFS Export - ", ExportDateTime),
           body = "lvmpd_cfs_markdown.html",
           html = TRUE,
